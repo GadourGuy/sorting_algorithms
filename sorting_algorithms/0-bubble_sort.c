fa9695f6851c92070cdb1fc6c80079d1b524d77c
root <root@MSI>
2024-01-18 12:23:58 +0200

    Pushing Bubble sort

---
+++ sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - An algorithm for sorting
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: Void Function
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j = 0;
+	int tmp;
+
+	if (!array || size <= 0)
+	{
+		return;
+	}
+	while (array[j + 1])
+	{
+		for (i = 0; i <= size - 2; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		j++;
+	}
+}
